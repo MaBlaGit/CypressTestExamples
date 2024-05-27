75f40afe4af7a421fc905d050b84b99736117157
MariuszBlajet <mariuszblajet@gmail.com>
2024-05-27 21:13:59 +0200

    chore: rename component

--- pages/account.page.ts
+++ pages/account.page.ts
@@ -1,11 +1,11 @@
 import { BasePage } from '@root/pages/base.page';
-import { HeaderPage } from '@root/components/header.component';
+import { HeaderComponent } from '@root/components/header.component';
 
 export class AccountPage extends BasePage {
 
     url = '/index.php?route=account/account'
 
-    headerComponent: HeaderPage = new HeaderPage();
+    headerComponent: HeaderComponent = new HeaderComponent();
 
     get myAccountNavButton() {
         return cy.get('#column-right a[href*="account/account"]');
