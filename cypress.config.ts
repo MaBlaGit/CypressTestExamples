68a5290dff9825a15e26f23f732700175bf41a05
MariuszBlajet <mariuszblajet@gmail.com>
2024-06-05 19:24:34 +0200

    chore: add retries

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,10 @@ module.exports = defineConfig({
   defaultCommandTimeout: 10000,
   viewportWidth: 1920,
   viewportHeight: 1080,
+  retries: {
+    runMode: 3,
+    openMode: 0
+  },
   e2e: {
     setupNodeEvents(on, config) {
       require('@cypress/grep/src/plugin')(on, config);
